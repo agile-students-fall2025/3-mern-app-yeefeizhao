ee17d7a1926b6488f163ee2c1935d4ac2a61f0fc
yeefeizhao <yz9614@nyu.edu>
2025-10-01 11:59:08 -0400

    about us page

---
+++ front-end/src/AboutUs.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import loadingIcon from './loading.gif'
+import axios from 'axios'
+import './AboutUs.css'
+
+function AboutUs() {
+  const [bio, setBio] = useState([])
+  const [loaded, setLoaded] = useState(false)
+  const [error, setError] = useState('')
+
+  const fetchBio = () => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_HOSTNAME}/bio`)
+      .then(response => {
+        const bio = response.data.bios[0]
+        setBio(bio)
+      })
+      .catch(err => {
+        const errMsg = JSON.stringify(err, null, 2)
+        setError(errMsg)
+      })
+      .finally(() => {
+        setLoaded(true)
+      })
+  }
+
+  useEffect(() => {
+    fetchBio()
+  }, [])
+
+  return (
+    <div className='about-us'>
+      {error && <p className="Messages-error">{error}</p>}
+
+      {bio.name ? <h2>{bio.name}</h2> : <h2>Your Name</h2>}
+      {!loaded && <img src={loadingIcon} alt="loading" />}
+
+      {bio.profilePicUrl ? <img src={bio.profilePicUrl} alt="" className='photo'/> : <></>}
+      {bio.introParagraph ? <p>{bio.introParagraph.join('\n\n')}</p> : <p>Your introduction paragraph about yourself...</p>}
+    </div>
+  )
+}
+
+export default AboutUs
